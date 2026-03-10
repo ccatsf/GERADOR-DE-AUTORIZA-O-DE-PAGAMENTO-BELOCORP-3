24185fab6ffd7106bc0fd56d46483d17f84cfb13
ccatsf <isabelcaloura2016@gmail.com>
2026-03-10 12:18:55 -0300

    Update firebase.ts

--- firebase.ts
+++ firebase.ts
@@ -1,7 +1,7 @@
 
-import { initializeApp } from 'firebase/app';
-import { getAuth, GoogleAuthProvider } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
+import { initializeApp } from 'firebase.ts/app';
+import { getAuth, GoogleAuthProvider } from 'firebase.ts/auth';
+import { getFirestore } from 'firebase.ts/firestore.rules';
 import firebaseConfig from '../firebase-applet-config.json';
 
 const app = initializeApp(firebaseConfig);
