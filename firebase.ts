f44bae9e4a6afe02a7e3aade92f7518ae58a4dba
ccatsf <isabelcaloura2016@gmail.com>
2026-03-10 12:48:21 -0300

    Update firebase.ts

--- firebase.ts
+++ firebase.ts
@@ -3,12 +3,13 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "SUA_API_KEY",
-  authDomain: "SEU_PROJETO.firebaseapp.com",
-  projectId: "SEU_PROJECT_ID",
-  storageBucket: "SEU_PROJETO.appspot.com",
-  messagingSenderId: "XXXXXXXX",
-  appId: "XXXXXXXX"
+  apiKey: "AIzaSyB9Fp-sy7d21sgdV2rm1usIxcPWV8QHHVI",
+  authDomain: "adad-a1674.firebaseapp.com",
+  projectId: "adad-a1674",
+  storageBucket: "adad-a1674.firebasestorage.app",
+  messagingSenderId: "123851363670",
+  appId: "1:123851363670:web:73d0252fbc1db8f3dbf2f7",
+  measurementId: "G-BTT0Y38ZWW"
 };
 
 const app = initializeApp(firebaseConfig);
