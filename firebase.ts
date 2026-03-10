58338204c8e3fbdefe3be53d60c5e4499f1dbc7a
ccatsf <isabelcaloura2016@gmail.com>
2026-03-10 12:35:19 -0300

    Update firebase.ts

--- firebase.ts
+++ firebase.ts
@@ -1,9 +1,18 @@
-
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-import config from "../firebase-applet-config.json";
-const app = initializeApp(config);
+
+const firebaseConfig = {
+  apiKey: "SUA_API_KEY",
+  authDomain: "SEU_PROJETO.firebaseapp.com",
+  projectId: "SEU_PROJECT_ID",
+  storageBucket: "SEU_PROJETO.appspot.com",
+  messagingSenderId: "XXXXXXXX",
+  appId: "XXXXXXXX"
+};
+
+const app = initializeApp(firebaseConfig);
+
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const googleProvider = new GoogleAuthProvider();
