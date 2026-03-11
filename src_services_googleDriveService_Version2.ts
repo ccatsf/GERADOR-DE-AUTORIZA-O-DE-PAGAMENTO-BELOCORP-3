ff94b57bbb8566a47a5d0e1adb27b5d0e2fbcc30
ccatsf <isabelcaloura2016@gmail.com>
2026-03-11 17:43:49 -0300

    Update src_services_googleDriveService_Version2.ts

--- src_services_googleDriveService_Version2.ts
+++ src_services_googleDriveService_Version2.ts
@@ -1,3 +1,4 @@
+console.log("Teste de Chave:", import.meta.env.VITE_GEMINI_API_KEY ? "OK" : "VAZIA");
 import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
 import { auth } from './firebase';
 
