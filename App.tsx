61fecb4109d03f0c1caa7656c750b39a563adf98
ccatsf <isabelcaloura2016@gmail.com>
2026-03-12 13:03:05 -0300

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import { uploadFileToDrive } from './src_services_googleDriveService_Version2';
+import { findOrCreateFolder, uploadFileToDrive } from './src_services_googleDriveService_Version2';
 import React, { useState, useRef, useEffect } from 'react';
 import { PaymentAuthData, INITIAL_AUTH_DATA, Beneficiary } from './types';
 import { parsePaymentText } from './services/geminiService.ts';
