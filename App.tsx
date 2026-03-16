b04cc25264bc7a09bf44a59ce256c1166518ea9e
ccatsf <isabelcaloura2016@gmail.com>
2026-03-16 12:47:30 -0300

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -56,8 +56,8 @@ const App: React.FC = () => {
     });
 
     // Escutar CRM para notificações de documentos faltando
-    const { db } = require('./firebase');
-    const { collection, onSnapshot } = require('firebase/firestore');
+    const { db } = import('./firebase');
+    const { collection, onSnapshot } = import('firebase/firestore');
     
     const unsubCRM = onSnapshot(collection(db, 'crm-cards'), (snapshot: any) => {
       let missing = 0;
